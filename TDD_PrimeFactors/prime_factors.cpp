3ea7cfcd22ef22b0b5a8963a31aa9f0617bc4cf2
HyunMin Cho <hm1041.cho@samsung.com>
2024-05-30 11:33:36 +0900

    [feature] page 37

--- TDD_PrimeFactors/prime_factors.cpp
+++ TDD_PrimeFactors/prime_factors.cpp
@@ -4,8 +4,11 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int a) {
+	vector<int> of(int number) {
 		vector<int> result = {};
+		if (number == 2) {
+			result.push_back(number);
+		}
 		return result;
 	}
 };
--- Test_PrimeFactors/test_prime_factors.cpp
+++ Test_PrimeFactors/test_prime_factors.cpp
@@ -10,3 +10,9 @@ TEST(PrimeFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(prime_factor.of(1), expected);
 }
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(prime_factor.of(2), expected);
+}
